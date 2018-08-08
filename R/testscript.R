d97f28f68a7e0f92ad257ec595bd0e99a22188a5
Geidelberg <lilygeidelberg@gmail.com>
2018-08-08 16:28:55 +0100

    trdtrd

--- R/testscript.R
+++ R/testscript.R
@@ -21,5 +21,27 @@ if (!require("ggplot2")) install.packages("ggplot2")
 
 # 2 height
 
-hist(rnorm(n = 1000, mean = 1.6, sd = 0.1), ylab = "", xlab = "Height in cm", main = "Distribution of adult height in cm")
-axis(side = 2, labels = F, tick = -0.01)
+
+hist(c(1.2, 2, rnorm(n = 10000, mean = 1.6, sd = 0.1)), nclass = 40, ylab = "", xlab = "Height in cm", main = "Distribution of adult height in cm", axes = F)
+axis(side = 1,  at=seq(1.2 , 2, 0.1), labels = seq(1.2 , 2, 0.1))
+
+
+
+# large SD
+
+hist(c(0, 100, rnorm(n = 100000, mean = 50, sd = 12)), nclass = 101, ylab = "Frequency", xlab = "", main = "", axes = F)
+axis(side = 1,  at=seq(0 , 100, 20), labels = seq(0 , 100, 20))
+
+
+
+# small SD
+
+hist(c(0, 101, rnorm(n = 100000, mean = 50, sd = 4)), nclass = 101, ylab = "Frequency", xlab = "", main = "", axes = F)
+axis(side = 1,  at=seq(0 , 100, 20), labels = seq(0 , 100, 20))
+
+
+# poisson SD
+
+
+barplot(table(rpois(n = 100000, lambda = 4)), axes = F)
+
